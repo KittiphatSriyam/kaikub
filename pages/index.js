00b36c59295696963daacf60aa34baf00724166d
Kittiphat Sriyam <skbjo032106@hotmail.com>
2022-06-07 22:22:48 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Kai Kub</title>
+        <meta name="description" content="Kai Kub" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+
+    </div>
+  )
+}
